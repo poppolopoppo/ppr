95f7e283836978a3ba2d55b4967f094353e08141
poppolopoppo <phamtom.x@gmail.com>
2026-07-21 10:32:05 +0200

    game: add TurboLarbin demo app with timeout and triangle shader
    main.cpp creates a TurboLarbin Application subclass that exits after 5
    seconds, using the new error_code lifecycle. triangle.slang provides a
    basic vertex/fragment shader. CMakeLists.txt adds a POST_BUILD step to
    copy shader assets to the output directory.

--- game/main.cpp
+++ game/main.cpp
@@ -1,11 +1,54 @@
+#include "pP/Macros.h"
+
 import engine.core;
 import engine.math;
 import engine.rhi;
 import engine.app;
 import std;
 
-int main(int argc, char *argv[]) {
+namespace demo {
     using namespace pP;
-    Application app("ppr", std::span<const char* const>(&argv[0], argc));
-    return app.run();
+    PPR_DEFINE_LOG_CATEGORY(Demo, info, none);
+
+    class TurboLarbin : public Application {
+    public:
+        using super_t = Application;
+        using super_t::super_t;
+
+    protected:
+        std::error_code initialize() override {
+            PPR_RETURN_ERROR_ON_FAIL(Demo, super_t::initialize());
+
+            m_started_at = time::now();
+
+            return default_value_v;
+        }
+
+        std::error_code update() override {
+            if (time::since(*m_started_at) > std::chrono::seconds(5)) {
+                return make_error_code(std::errc::timed_out);
+            }
+
+            PPR_RETURN_ERROR_ON_FAIL(Demo, super_t::update());
+
+            return default_value_v;
+        }
+
+        std::error_code shutdown() noexcept override {
+            m_started_at.reset();
+
+            PPR_RETURN_ERROR_ON_FAIL(Demo, super_t::shutdown());
+
+            return default_value_v;
+        }
+
+    private:
+        std::optional<TimePoint> m_started_at{};
+    };
+}
+
+int main(const int argc, char *argv[]) {
+    demo::TurboLarbin app("ppr", std::span(&argv[0], argc));
+    const std::error_code err = app.run();
+    return err.value();
 }
