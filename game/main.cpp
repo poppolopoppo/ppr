5fc7b0e7c88dd302ce94fb8b4da502bdf519e3cf
pOpPoLoPoPpo <poppolopoppo@gmail.com>
2026-09-22 22:52:20 +0200

    app.game: fix argv span sign conversion

--- game/main.cpp
+++ game/main.cpp
@@ -54,7 +54,7 @@ namespace demo {
 }
 
 int main(const int argc, char *argv[]) {
-    demo::TurboLarbin app("ppr", std::span(&argv[0], argc));
+    demo::TurboLarbin app("ppr", std::span(&argv[0], pP::checked_cast<std::size_t>(argc)));
     const std::error_code err = app.run();
     return err.value();
 }
